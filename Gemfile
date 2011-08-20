a14b51c942cadf523c762af1ed05c9c8c689594a
Tom Armitage <tom@infovore.org>
2011-08-20 17:03:29 +0100

    Initial Rails commit.

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+gem 'mysql'
+gem 'haml'
+
+# Deploy with Capistrano
+gem 'capistrano'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ghostcar::Application.config.secret_token = '41ab3da5ae482d362fd6c975ca9392d8c1fb6c095d50e31174a9fe843d4a863277a3dec10f956f98527e6fe2e5e50b391c03e51769fec468d4969e1a4ee5fe26'
