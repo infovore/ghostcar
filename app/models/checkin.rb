6519614b4278b7cb72ed2eda9c670e9570d75810
Tom Armitage <tom@infovore.org>
2011-08-21 13:27:39 +0100

    Incremental checkins.

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,15 +1,37 @@
 class Checkin < ActiveRecord::Base
   belongs_to :user
 
-  def self.ingest_checkins_for_user(user)
+  def self.ingest_all_checkins_for_user(user)
     checkin_data = user.all_foursq_checkins
     checkin_data.each do |raw_checkin|
-      user.checkins.create(:checkin_id => raw_checkin['id'],
-                           :timezone => raw_checkin['timeZone'],
-                           :timestamp => raw_checkin['createdAt'],
-                           :shout => raw_checkin['shout'],
-                           :venue_id => raw_checkin['venue']['id'],
-                           :venue_name => raw_checkin['venue']['name'])
+      create_for_user_from_json(user, raw_checkin)
     end
   end
+  
+  def self.ingest_latest_checkins_for_user(user)
+    if user.checkins.any?
+      latest_timestamp = user.checkins.last.timestamp
+      # we add 1 to ignore the record we derived this from.
+      latest_timestamp = latest_timestamp.to_i + 1
+
+      checkin_data = user.all_foursq_checkins_since(latest_timestamp)
+      checkin_data.each do |raw_checkin|
+        create_for_user_from_json(user, raw_checkin)
+      end
+    end
+  end
+
+  private
+
+  def self.create_for_user_from_json(user,json)
+    unless user.checkins.where(:checkin_id => json['id']).any?
+      user.checkins.create(:checkin_id => json['id'],
+                           :timezone => json['timeZone'],
+                           :timestamp => json['createdAt'],
+                           :shout => json['shout'],
+                           :venue_id => json['venue']['id'],
+                           :venue_name => json['venue']['name'])
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,20 @@ class User < ActiveRecord::Base
   require 'addressable/uri'
   require 'open-uri'
 
-  has_many :checkins
+  has_many :checkins, :order => :timestamp
 
   def name
     "#{firstname} #{lastname}"
   end
 
-  def all_foursq_checkins
+  def all_foursq_checkins(options = {})
     checkins_url = "https://api.foursquare.com/v2/users/self/checkins"
     count = 100
     offset = 0
     items = []
     while count == 100
       uri = Addressable::URI.new
-      uri.query_values = {:oauth_token => access_token, :limit => count.to_s, :offset => offset.to_s}
+      uri.query_values = {:oauth_token => access_token, :limit => count.to_s, :offset => offset.to_s}.merge(options)
       full_path = checkins_url + "?" + uri.query
       raw_json = open(full_path).read
       data = JSON.parse(raw_json)
@@ -25,5 +25,9 @@ class User < ActiveRecord::Base
       offset = offset + count
     end
     items
+  end
+
+  def all_foursq_checkins_since(timestamp)
+    all_foursq_checkins("afterTimestamp" => t.to_s)
   end
 end
