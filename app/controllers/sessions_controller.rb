26829b41114055e205de2ce532734c27f2d4aa02
Tom Armitage <tom@infovore.org>
2011-08-30 22:17:05 +0100

    We handle secondary authentication like this.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,19 +6,35 @@ class SessionsController < ApplicationController
 
   def callback
     code = params[:code]
-    @access_token = foursquare.access_token(code, callback_session_url)
-    session[:access_token] = @access_token
+    if !current_gc_user
+      @access_token = foursquare.access_token(code, callback_session_url)
+      session[:access_token] = @access_token
 
-    @user = User.find_or_create_by_access_token(@access_token)
-    u = current_user
-    if @user.foursquare_id.blank?
-      @user.update_attributes(:firstname => u.first_name,
-                              :lastname => u.last_name,
-                              :foursquare_id => u.id,
-                              :picture_url=> u.photo)
-    end 
+      @user = User.find_or_create_by_access_token(@access_token)
+      u = current_user
+      if @user.foursquare_id.blank?
+        @user.update_attributes(:firstname => u.first_name,
+                                :lastname => u.last_name,
+                                :foursquare_id => u.id,
+                                :picture_url=> u.photo)
+      end 
+    else
+      # we're authenticating the second account
+      @user = current_gc_user
+
+      @secondary_access_token = foursquare.access_token(code, callback_session_url)
+      @user.secondary_access_token = @secondary_access_token
+
+      secondary_foursquare= Foursquare::Base.new(@secondary_access_token)
+      @secondary_user = secondary_foursquare.users.find("self")
+      
+      @user.secondary_foursquare_id = @secondary_user.id
+
+      @user.save
+    end
     redirect_to pages_path
   end
+
 
   def logout
     session[:access_token] = nil
