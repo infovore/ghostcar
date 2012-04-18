e7cee98cc6eb2a8e5d762343ad7a3e4c242442c3
Tom Armitage <tom@infovore.org>
2012-04-18 16:38:02 +0100

    All-view on pagination.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,13 @@ class PagesController < ApplicationController
     if current_gc_user
       t = Time.now - 1.year
       @last_year = current_gc_user.next_checkin_after(t)
+      if params[:show] == "all"
+        @show_all = true
+        @checkins = Checkin.where(:user_id => current_gc_user.id).page(params[:page])
+      else
+        @checkins = current_gc_user.latest_checkins
+      end
+      
     end
   end
 end
